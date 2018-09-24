ef6f66d8c26ab39936ca89b95339e0ff56c49761
Victor-733 <victorwahid@gmail.com>
2018-09-24 11:31:32 +0000

    change .py slightly and created styles and forside.tpl

--- midannarverkefni.py
+++ midannarverkefni.py
@@ -3,9 +3,12 @@ from sys import argv
 import json
 import urllib.request
 
+with urllib.request.urlopen("http://apis.is/petrol/") as url:
+    data = json.loads(url.read().decode())
+
 @route("/")
 def index():
-    return "myes"
+    return template("forsida.tpl", data=data)
 
 
 run(host="0.0.0.0", port=argv[1], debug=True, reloader=True)
